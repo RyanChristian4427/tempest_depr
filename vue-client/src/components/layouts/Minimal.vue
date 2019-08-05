ff20abeedd1a9ab90e7c43fa035198ed7d1c4eb1
Ryan Christian <ryanchristian4427@gmail.com>
2019-08-05 00:34:19 -0500

    Added basic login page

---
+++ vue-client/src/components/layouts/Minimal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="layout-content">
+        <router-view class="page-content"></router-view>
+        <core-footer></core-footer>
+    </div>
+</template>
+
+<script lang="ts">
+    import CoreFooter from '@/components/core/Footer.vue';
+
+    export default {
+        name: 'Login-Layout',
+        components: {
+            CoreFooter,
+        },
+    };
+</script>
+
+<style lang="scss">
+    .layout-content {
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+    }
+    .page-content {
+        flex: 1;
+    }
+</style>
---
+++ vue-client/src/views/authentication/Login.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <section class="hero is-medium is-bold">
+            <div class="hero-body">
+                <h1 class="title">
+                    Login
+                </h1>
+            </div>
+        </section>
+        <section>
+            <form class="card" id="logon-form">
+                <div class="field">
+                    <label for="email" class="label">Email</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input type="email" id="email" placeholder="Email input">
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="password" class="label">Password</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input type="password" id="password" placeholder="Password">
+                    </div>
+                </div>
+            </form>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+    };
+</script>
+
+<style lang="scss">
+    html {
+        overflow-y: hidden;
+    }
+
+    .hero {
+        background: $theme-primary;
+        height: 33vh;
+    }
+
+    .hero .title {
+        margin-left:25vw;
+        color: $theme-background-primary;
+    }
+
+    .card {
+        background: $theme-secondary;
+        width: 50vw;
+        margin: 5vh 25vw;
+    }
+
+    #logon-form {
+        padding: 5vh 5vw 5vh;
+    }
+</style>
