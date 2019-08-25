7ce625062edd39330245892b220947d646053e74
Ryan Christian <ryanchristian4427@gmail.com>
2019-08-25 03:03:06 -0500

    Added new user state, and cleaned auth module

--- vue-client/src/store/actions/auth.ts
+++ vue-client/src/store/actions/auth.ts
@@ -1,4 +1,5 @@
 export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const AUTH_FIRST_TIME = 'AUTH_FIRST_TIME';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const AUTH_LOGOUT = 'AUTH_LOGOUT';
