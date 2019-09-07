70c1becdff2292b5198f1cbea197c1332653192b
Ryan Christian <ryanchristian4427@gmail.com>
2019-09-07 17:28:14 -0500

    Added test data to db migration script

--- rocket-server/migrations/2019-09-02-090136_user_options/up.sql
+++ rocket-server/migrations/2019-09-02-090136_user_options/up.sql
@@ -4,3 +4,5 @@ CREATE TABLE user_options
     emails_per_page      INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+INSERT INTO user_options (user_id, emails_per_page) VALUES (1, 50);
