62eb6c3d16f46d32117fee9cf870a7e3b8736476
Ryan Christian <ryanchristian4427@gmail.com>
2019-09-07 16:23:15 -0500

    Fixed server db connection to match proper format

--- rocket-server/src/services/user_service.rs
+++ rocket-server/src/services/user_service.rs
@@ -1,8 +1,6 @@
-use crate::db::user_options_repository;
+use crate::db::{Conn, user_options_repository};
 use crate::models::user_options::UserOptions;
 
-use diesel::pg::PgConnection;
-
-pub fn get_options(user_id: i32, conn: &PgConnection) -> Option<UserOptions> {
-    user_options_repository::get_options(user_id, &conn)
+pub fn get_options(user_id: i32, conn: Conn) -> Option<UserOptions> {
+    user_options_repository::get_options(user_id, conn)
 }
