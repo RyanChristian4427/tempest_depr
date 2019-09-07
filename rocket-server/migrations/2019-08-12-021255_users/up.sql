d0a25ae61eb3cdf6c79202fb6d7a118ceb157e3d
Ryan Christian <ryanchristian4427@gmail.com>
2019-09-07 17:28:14 -0500

    Added test data to db migration script

--- rocket-server/migrations/2019-08-12-021255_users/up.sql
+++ rocket-server/migrations/2019-08-12-021255_users/up.sql
@@ -10,3 +10,8 @@ CREATE TABLE users
 --     expiry_datetime      TIMESTAMP WITH TIME ZONE,
     CONSTRAINT unique_email UNIQUE (email)
 );
+
+ALTER SEQUENCE users_id_seq RESTART WITH 1;
+
+INSERT INTO users (first_name, last_name, email, hashed_password)
+VALUES ('smoke', 'test', 'smoketest@example.com', '$rscrypt$0$DggB$AFR6Zk+AY16xtD9KAor9tw==$k7V3U+gadVDLCWwS41qwd09pt0x/WFDtidUj/S8MhJ4=$');
