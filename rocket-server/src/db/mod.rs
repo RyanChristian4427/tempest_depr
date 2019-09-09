cbdd7c2fb8229c99fcf7c2bc2946523a7a621eee
Ryan Christian <ryanchristian4427@gmail.com>
2019-09-08 19:59:02 -0500

    refactor(auth-repository): Renamed 'user-repository' to 'auth-repository'

--- rocket-server/src/db/mod.rs
+++ rocket-server/src/db/mod.rs
@@ -1,7 +1,7 @@
 use rocket_contrib::databases::diesel;
 
 pub mod user_options_repository;
-pub mod users_repository;
+pub mod auth_repository;
 
 #[database("diesel_postgres_pool")]
 pub struct Conn(diesel::PgConnection);
