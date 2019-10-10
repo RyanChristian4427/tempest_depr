e39cc6f304e27e84f6389f1b2fd55c995d70039a
Ryan Christian <ryanchristian4427@gmail.com>
2019-10-10 15:21:23 +0100

    feat(db-schema): Added a new table for email storage

--- rocket-server/src/schema.rs
+++ rocket-server/src/schema.rs
@@ -1,4 +1,14 @@
 table! {
+    mailbox_messages (uid) {
+        uid -> Int4,
+        mailbox_id -> Int4,
+        sender -> Text,
+        content -> Text,
+        datetime -> Date,
+    }
+}
+
+table! {
     user_options (user_id) {
         user_id -> Int4,
         emails_per_page -> Int4,
@@ -15,9 +25,11 @@ table! {
     }
 }
 
+joinable!(mailbox_messages -> users (mailbox_id));
 joinable!(user_options -> users (user_id));
 
 allow_tables_to_appear_in_same_query!(
+    mailbox_messages,
     user_options,
     users,
 );
