1e81ea8e106f9d04fd0865c062ee001f873b974b
Ryan Christian <ryanchristian4427@gmail.com>
2019-10-10 19:21:22 +0100

    feat(email-service): Added service to retrieve all emails and prepare them to be sent

--- rocket-server/src/services/user_service.rs
+++ rocket-server/src/services/user_service.rs
@@ -1,6 +1,21 @@
-use crate::db::{user_options_repository, Conn};
+use crate::db::{email_repository, user_options_repository, Conn};
+use crate::models::email::EmailJson;
 use crate::models::user_options::UserOptions;
 
 pub fn get_options(user_id: i32, conn: Conn) -> Option<UserOptions> {
     user_options_repository::get_options(user_id, conn)
+}
+
+pub fn get_inbox(user_id: i32, conn: Conn) -> Option<Vec<EmailJson>> {
+    let email_list = email_repository::get_emails(user_id, conn).unwrap();
+    if !email_list.is_empty() {
+        Some(
+            email_list
+                .into_iter()
+                .map(|email| email.to_email_json())
+                .collect(),
+        )
+    } else {
+        None
+    }
 }
