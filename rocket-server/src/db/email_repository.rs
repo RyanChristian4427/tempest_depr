e8066e8fbb7372a05fd8e12f673554e8db06421d
Ryan Christian <ryanchristian4427@gmail.com>
2019-10-10 19:20:38 +0100

    refactor: Removed logic from email repository, as it was in poor form

--- rocket-server/src/db/email_repository.rs
+++ rocket-server/src/db/email_repository.rs
@@ -1,23 +1,12 @@
 use crate::db::Conn;
-use crate::models::email::{Email, EmailJson};
+use crate::models::email::Email;
 use crate::schema::mailbox_messages;
 
 use diesel::prelude::*;
 
-pub fn get_emails(user_id: i32, conn: Conn) -> Option<Vec<EmailJson>> {
-    let result = mailbox_messages::table
+pub fn get_emails(user_id: i32, conn: Conn) -> Option<Vec<Email>> {
+    mailbox_messages::table
         .filter(mailbox_messages::mailbox_id.eq(user_id))
         .get_results::<Email>(&*conn)
-        .expect("Cannot find inbox for user");
-
-    if !result.is_empty() {
-        Some(
-            result
-                .into_iter()
-                .map(|email| email.to_email_json())
-                .collect(),
-        )
-    } else {
-        None
-    }
+        .ok()
 }
