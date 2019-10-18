ed8481dfdb87914c603eee5bcbfa726c4ddf9ad7
Ryan Christian <ryanchristian4427@gmail.com>
2019-10-18 19:07:39 +0100

    feat: Basic tiles can be added to the side menu

---
+++ vue-client/src/components/tempest/side-nav/NavTile.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="nav-tile level">
+        <h3 class="subtitle is-size-4"> <b>{{ navTile.title }}</b> </h3>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Vue, Component, Prop} from 'vue-property-decorator';
+    import {SideNavTile} from '@/models/SideNav';
+
+    @Component
+    export default class NavTile extends Vue {
+        @Prop()
+        public navTile: SideNavTile | undefined;
+    }
+</script>
+
+<style scoped lang="scss">
+    .nav-tile {
+        padding-top: 1vh;
+        padding-bottom: 1vh;
+        padding-left: 1.5vw;
+        margin-right: 1vw;
+
+        .subtitle {
+            color: $theme-isabelline
+        }
+    }
+
+    .level {
+        margin-bottom: 0 !important;
+    }
+
+    .nav-tile:hover {
+        cursor: pointer;
+        background: $theme-charleston-green;
+        border-bottom-right-radius: 30px;
+        border-top-right-radius: 30px;
+    }
+
+</style>
