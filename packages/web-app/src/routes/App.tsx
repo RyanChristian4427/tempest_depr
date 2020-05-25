117c134fca331a718bb95ff02fa27b9b14a26501
Ryan Christian <ryanchristian4427@gmail.com>
2020-05-25 16:14:16 +0100

    feat: Pushing changes to new app off my system

--- packages/web-app/src/routes/App.tsx
+++ packages/web-app/src/routes/App.tsx
@@ -1,28 +1,43 @@
 import { FunctionalComponent, h } from 'preact';
-import { Route, Router, RouterOnChangeArgs } from 'preact-router';
+import { Suspense } from 'preact/compat';
+import { Route, Router } from 'preact-router';
 
-import Home from './home';
-import Profile from './profile';
-import NotFoundPage from './notfound';
-import Header from '../components/header';
+import Login from './auth/login';
+import Notfound from './notfound';
 
 const App: FunctionalComponent = () => {
-    let currentUrl: string;
-    const handleRoute = (e: RouterOnChangeArgs) => {
-        currentUrl = e.url;
-    };
-
     return (
         <div id="app">
-            <Header />
-            <Router onChange={handleRoute}>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
+            <Suspense fallback={<Fallback />}>
+                <Router>
+                    <Route path="/login" component={Login} />
+                    <Notfound default />
+                </Router>
+            </Suspense>
         </div>
     );
 };
+
+const Fallback: FunctionalComponent = () => {
+    return (
+        <div class="w-screen block">
+            <div class="flex">
+                <div class="main-content" />
+            </div>
+        </div>
+    );
+};
+
+// const AuthenticatedRoute = (props: { path: string; component: FunctionalComponent }): VNode => {
+//     const isLoggedIn = useStore().authStore.isAuthenticated;
+//
+//     useEffect(() => {
+//         if (!isLoggedIn) route('/login', true);
+//     }, [isLoggedIn]);
+//
+//     if (!isLoggedIn) return null;
+//
+//     return <Route {...props} />;
+// };
 
 export default App;
--- packages/web-app/src/routes/auth/login.tsx
+++ packages/web-app/src/routes/auth/login.tsx
@@ -1,7 +1,26 @@
 import { FunctionalComponent, h } from 'preact';
 
 const Login: FunctionalComponent = () => {
-    return <div>Hello World</div>;
+    return (
+        <div class="auth-page">
+            <div class="main-content">
+                <div class="columns">
+                    <div class="column is-7 first-column">
+                        <section class="section has-text-centered">
+                            <div class="container">
+                                <h1 class="title has-text-white-bis">Welcome Back!</h1>
+                                <h1 class="subtitle has-text-white-bis">Login to get back to your email</h1>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="column is-5 second-column">
+                        <div>Title2</div>
+                        <div>Hello World1</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
