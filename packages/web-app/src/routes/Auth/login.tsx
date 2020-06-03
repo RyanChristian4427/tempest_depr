285be72f9a3c9ef68eed67cb84c57a432a03c2a0
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-03 18:46:57 +0100

    feat: Login page layout + mobile view complete

--- packages/web-app/src/routes/Auth/login.tsx
+++ packages/web-app/src/routes/Auth/login.tsx
@@ -4,20 +4,7 @@ const Login: FunctionalComponent = () => {
     return (
         <div class="auth-page">
             <div class="main-content">
-                <div class="columns">
-                    <div class="column is-7 first-column">
-                        <section class="section has-text-centered">
-                            <div class="container">
-                                <h1 class="title has-text-white-bis">Welcome Back!</h1>
-                                <h1 class="subtitle has-text-white-bis">Login to get back to your email</h1>
-                            </div>
-                        </section>
-                    </div>
-                    <div class="column is-5 second-column">
-                        <div>Title2</div>
-                        <div>Hello World1</div>
-                    </div>
-                </div>
+                <div class="login-card">hello</div>
             </div>
         </div>
     );
